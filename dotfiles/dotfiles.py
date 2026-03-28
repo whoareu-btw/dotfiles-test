fbfa4573e179547112e2bb22586f8878a8ac1675
Neko-chi <orangganteng87659@gmail.com>
2026-03-28 20:08:48 +0700

    still experimental

---
+++ dotfiles/dotfiles.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+import shutil
+
+HOME = Path.home()
+DOTFILES = Path("dotfiles")
+
+configs = [
+        "sway",
+        "foot",
+        "alacritty",
+        "waybar",
+        "wlogout",
+        "rofi",
+        "fuzzel",
+        "nvim",
+]
+
+for cfg in configs:
+    src = DOTFILES / cfg
+    dst = HOME / ".config" / cfg
+
+    print(f"Applying {cfg}...")
+
+    dst.mkdir(parents=True, exist_ok=True)
+    shutil.copytree(src, dst, dirs_exist_ok=True)
+
+print("Done!")
+
+wall_src = DOTFILES / "Wallpapers"
+wall_dst = HOME / "Wallpapers"
+
+wall_dst.mkdir(parents=True, exist_ok=True)
+
+for file in wall_src.iterdir():
+    if file.is_file():
+        shutil.copy(file, wall_dst)
+
+print("Wallpaper copied")
+
+shutil.copy(
+        DOTFILES / "zsh/.zshrc",
+        HOME / ".zshrc"
+)
---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -euo pipefail
+
+LOG_FILE="another-dotfiles$(date +%Y%m%d_%H%M).log"
+
+log_message() {
+	echo -e "[$(date +'%Y-%m-%d %H:%M:%S') $1]" | tee -a "$LOG_FILE"
+}
+
+if ! command -v python3 &> /dev/null; then
+	echo "Python not found, installing (╥﹏╥)"
+
+	if command -v apk &> /dev/null; then
+		sudo apk -U upgrade
+		sudo apk add python
+
+	elif command -v pacman &> /dev/null; then
+		sudo pacman -Syu
+		sudo pacman -S python
+
+	elif command -v apt &> /dev/null; then
+		sudo apt update && sudo apt upgrade
+		sudo apt install python3
+
+	elif command -v dnf &> /dev/null; then
+		sudo dnf update
+		sudo dnf install python
+
+	else
+		echo "Unsupported distro"
+		exit 1
+	fi
+fi
+
+echo "Running Python script"
+python3 packages/packages.py
+
+echo "Applying dotfiles"
+python3 dotfiles/dotfiles.py
